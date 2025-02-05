f1366f845a43ca4db4e4baa3cba118a614836ca3
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2025-02-05 19:54:19 -0300

    Fix equality comparison in generate_templates.sh

--- scripts/generate_templates.sh
+++ scripts/generate_templates.sh
@@ -20,7 +20,7 @@ cd $(dirname $0)/../cpython/Doc/locales
 
 # Python 3.7: Add missing sections to satisfy Blurb's check
 # Useful if setup.sh was not ran
-if [[ ${PYDOC_VERSION} == '3.7' ]] && [ ! -f ../../114553.patch ]; then
+if [ ${PYDOC_VERSION} = '3.7' ] && [ ! -f ../../114553.patch ]; then
   curl -L https://github.com/python/cpython/pull/114553.patch -o ../../114553.patch
   git apply ../../114553.patch
 fi
