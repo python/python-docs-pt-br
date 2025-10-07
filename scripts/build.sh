7e5b599ee8a419c36cc6cfd7ea7d2f4978023ae9
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2025-10-07 20:08:00 -0300

    Enable PDF and EPUB build (#272)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-# Build translated docs to pop up errors
+# Build translated docs
+# Expects input like 'html' and 'latex', defaults to 'html'.
 #
 # SPDX-License-Identifier: CC0-1.0
 
 set -xeu
+
+if [ -z "$1" ]; then
+  format=html
+else
+  format="$1"
+fi
 
 # Fail earlier if required variables are not set
 test -n ${PYDOC_LANGUAGE+x}
@@ -14,15 +21,15 @@ mkdir -p logs
 # If version is 3.12 or older, set gettext_compact.
 # This confval is not needed since 3.12.
 # In 3.13, its presence messes 3.13's syntax checking (?)
-opts="-D language=${PYDOC_LANGUAGE} --keep-going -w ../../logs/sphinxwarnings.txt"
+opts="-D language=${PYDOC_LANGUAGE} --keep-going -w ../../logs/sphinxwarnings-${format}.txt"
 minor_version=$(git -C cpython/Doc branch --show-current | sed 's|^3\.||')
 if [ $minor_version -lt 12 ]; then
   opts="$opts -D gettext_compact=False"
 fi
 
-make -C cpython/Doc html SPHINXOPTS="${opts}"
+make -C cpython/Doc "${format}" SPHINXOPTS="${opts}"
 
 # Remove empty file
-if [ ! -s logs/sphinxwarnings.txt ]; then
-  rm logs/sphinxwarnings.txt
+if [ ! -s "logs/sphinxwarnings-${format}.txt" ]; then
+  rm "logs/sphinxwarnings-${format}.txt"
 fi
